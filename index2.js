1b6026a22bc0282b332ebcb42d6723122188ea56
Madhav Vishist <mv252670@gmail.com>
2024-10-14 22:09:08 +0530

    /get route added

--- index2.js
+++ index2.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const cors = require('cors');
-const { Pool } = require('pg');
+const PORT = 7000;
 
 // Setup the Express server
 const app = express();
-app.use(cors());
 app.use(express.json());
 
 // Set up the connection to PostgreSQL using Pool
-
+app.get('/',(req, res)=>{
+    res.json({message : "testing this web"})
+})
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     
